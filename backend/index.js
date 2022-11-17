37956de30c3996ac1fa6bb626b446d69cb9422fa
eshans <eshans2000@gmail.com>
2022-11-17 09:14:23 +0530

    change backend port

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,20 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response
   res.end(); //end the response
-}).listen(3000, function(){
- console.log("server start at port 3000"); //the server object listens on port 3000
+}).listen(8000, function(){
+ console.log("server start at port 3000");
+ var url = req.url;
+ if(url ==='/about'){
+    res.write('<h1>about us page<h1>'); //write a response
+    res.end(); //end the response
+ }else if(url ==='/contact'){
+    res.write('<h1>contact us page<h1>'); //write a response
+    res.end(); //end the response
+ }else{
+    res.write('<h1>Hello World!<h1>'); //write a response
+    res.end(); //end the response
+ }
+ 
+ 
+ //the server object listens on port 3000
 });
