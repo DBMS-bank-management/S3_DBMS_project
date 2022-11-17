d35e37dbaf0168f913617372cce27582aecb3722
eshans <eshans2000@gmail.com>
2022-11-17 09:22:41 +0530

    fix server start error

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
 var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response
-  res.end(); //end the response
+
+    var url = req.url;
+    if(url ==='/about'){
+       res.write('<h1>about us page<h1>'); //write a response
+       res.end(); //end the response
+    }else if(url ==='/contact'){
+       res.write('<h1>contact us page<h1>'); //write a response
+       res.end(); //end the response
+    }else{
+       res.write('<h1>Hello World!<h1>'); //write a response
+       res.end(); //end the response
+    }
 }).listen(8000, function(){
  console.log("server start at port 3000");
- var url = req.url;
- if(url ==='/about'){
-    res.write('<h1>about us page<h1>'); //write a response
-    res.end(); //end the response
- }else if(url ==='/contact'){
-    res.write('<h1>contact us page<h1>'); //write a response
-    res.end(); //end the response
- }else{
-    res.write('<h1>Hello World!<h1>'); //write a response
-    res.end(); //end the response
- }
+ 
  
  
  //the server object listens on port 3000
