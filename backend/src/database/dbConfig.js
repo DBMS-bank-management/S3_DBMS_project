33db2361372a3a631bf782fde1bfa6f7ae065b69
eshans <eshans2000@gmail.com>
2022-12-24 23:08:03 +0530

    new dbconfig

--- backend/src/database/dbConfig.js
+++ backend/src/database/dbConfig.js
@@ -1,13 +1,6 @@
-// module.exports = {
-//   HOST: "sql.freedb.tech",
-//   USER: "freedb_DBMS_FREEDB",
-//   PASSWORD: "q9H@E?Z3vbm$t$$",
-//   DB: "freedb_DBMS_1"
-// };
-
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "CseTrack10",
-    DB: "bank_02"
-  };
+  HOST: "sql.freedb.tech",
+  USER: "freedb_DBMS_FREEDB",
+  PASSWORD: "yZ9AKbH!EbgrFJ9",
+  DB: "freedb_bank_02"
+};
