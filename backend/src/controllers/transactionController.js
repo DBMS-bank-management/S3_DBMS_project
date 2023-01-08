7611acee53e4996af4960cbaa12752e3d957a6d0
eshans <eshans2000@gmail.com>
2023-01-08 14:26:54 +0530

    add transaction WIP

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -11,10 +11,10 @@ exports.create = (req, res) => {
 
   // Create a TransactionModel
   const transaction = new TransactionModel({
-    trans_ID : req.body.trans_ID,
-    amount:req.body.amount,
-    mode_ID:req.body.mode_ID,
-    acc_ID :req.body.acc_ID, 
+    trans_ID: req.body.trans_ID,
+    amount: req.body.amount,
+    mode_ID: req.body.mode_ID,
+    acc_ID: req.body.acc_ID,
     description: req.body.description,
   });
 
@@ -23,7 +23,8 @@ exports.create = (req, res) => {
     if (err)
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the TransactionModel.",
+          err.message ||
+          "Some error occurred while creating the TransactionModel.",
       });
     else res.send(data);
   });
@@ -36,7 +37,8 @@ exports.findAll = (req, res) => {
   TransactionModel.getAll(title, (err, data) => {
     if (err)
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving transaction.",
+        message:
+          err.message || "Some error occurred while retrieving transaction.",
       });
     else res.send(data);
   });
@@ -69,19 +71,24 @@ exports.update = (req, res) => {
 
   // console.log(req.body);
 
-  TransactionModel.updateById(req.params.id, new TransactionModel(req.body), (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found transaction with trans_ID ${req.params.id}.`,
-        });
-      } else {
-        res.status(500).send({
-          message: "Error updating transaction with trans_ID " + req.params.id,
-        });
-      }
-    } else res.send(data);
-  });
+  TransactionModel.updateById(
+    req.params.id,
+    new TransactionModel(req.body),
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found transaction with trans_ID ${req.params.id}.`,
+          });
+        } else {
+          res.status(500).send({
+            message:
+              "Error updating transaction with trans_ID " + req.params.id,
+          });
+        }
+      } else res.send(data);
+    }
+  );
 };
 
 exports.delete = (req, res) => {
@@ -93,7 +100,8 @@ exports.delete = (req, res) => {
         });
       } else {
         res.status(500).send({
-          message: "Could not delete transaction with trans_ID " + req.params.id,
+          message:
+            "Could not delete transaction with trans_ID " + req.params.id,
         });
       }
     } else res.send({ message: `transaction was deleted successfully!` });
@@ -101,14 +109,28 @@ exports.delete = (req, res) => {
 };
 
 //find by account number???
-exports.getTransactionsByUserID= (req, res) => {
-  const name = null //req.query.name;
+exports.getTransactionsByUserID = (req, res) => {
+  const name = null; //req.query.name;
 
   TransactionModel.findTransactionsByUserId(req.params.id, (err, data) => {
     if (err)
       res.status(500).send({
-        message: err.message || "Some error occurred while retrieving transactions.",
+        message:
+          err.message || "Some error occurred while retrieving transactions.",
       });
     else res.send(data);
   });
+};
+
+exports.addTransfer = (req, res) => {
+
+  TransactionModel.addTransfer(req.body, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving transactions.",
+      });
+    else res.send(data);
+  });
+
 };
--- frontend/src/pages/employeePortal/transaction/addTransaction.js
+++ frontend/src/pages/employeePortal/transaction/addTransaction.js
@@ -1,100 +1,126 @@
-import React, { useState } from "react";
-import { Select } from "antd";
-import { addTransaction } from "../../../api/transaction";
+import React, { useEffect, useState } from "react";
+import {
+  InputNumber,
+  message,
+  Select,
+  Button,
+  Checkbox,
+  Form,
+  Input,
+} from "antd";
+import { addTransaction, addTransfer } from "../../../api/transaction";
 import { EmployeePageHeading } from "../../../components/layout/employeePageHeading";
-import { Button, Checkbox, Form, Input } from "antd";
+import { getAccounts } from "../../../api/account";
 
 const handleChange = (value) => {
   console.log(`selected ${value}`);
 };
 
 const onFinish = (formValues) => {
+  addTransfer(formValues)
+    .then(() => {
+      message.success("Transfer done");
+    })
+    .catch((err) => {
+      console.log({ err });
+      message.error("Transfer Error!");
+    });
   console.log({ formValues });
 };
 
-const AddTransaction = () => (
-  <div>
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="From Account "
-        name="FromAccount"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Select
-          // initialValues="lucy"
-          style={{
-            width: 200,
-          }}
-          onChange={handleChange}
-          options={[
-            {
-              label: "Jack",
-              value: "jack",
-            },
-            {
-              label: "Lucy",
-              value: "lucy",
-            },
-          ]}
-        />
-      </Form.Item>
-      <Form.Item
-        label="To Account"
-        name="ToAccount"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Select
-          // defaultValue="lucy"
-          style={{
-            width: 200,
-          }}
-          onChange={handleChange}
-          options={[
-            {
-              label: "To Account",
-              options: [
-                {
-                  label: "Jack",
-                  value: "jack",
-                },
-                {
-                  label: "Lucy",
-                  value: "lucy",
-                },
-              ],
-            },
-          ]}
-        />
-      </Form.Item>
+const AddTransaction = () => {
+  const [accounts, setAccounts] = useState();
+  const [loading, setLoading] = useState(true);
 
-      <Form.Item
-        label="Amount"
-        name="Amount"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
+  useEffect(() => {
+    setLoading(true);
+    loadAccountsList();
+  }, []);
+
+  function loadAccountsList() {
+    getAccounts()
+      .then((data) => {
+        setAccounts(
+          data.map((acc) => ({ label: acc.account_ID, value: acc.account_ID }))
+        );
+      })
+      .catch((err) => message.error(err))
+      .finally(() => setLoading(false));
+  }
+
+  return (
+    <div>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        onFinish={onFinish}
       >
-        <Button type="primary" htmlType="submit">
-          Done
-        </Button>
-      </Form.Item>
-    </Form>
-  </div>
-);
+        <Form.Item
+          label="From Account "
+          name="FromAccount"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Select
+            // initialValues="lucy"
+            style={{
+              width: 200,
+            }}
+            showSearch
+            filterOption={
+              (input, option) => option.label === input
+              // (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+            }
+            onChange={handleChange}
+            options={accounts}
+          />
+        </Form.Item>
+        <Form.Item
+          label="To Account"
+          name="ToAccount"
+          rules={[
+            { required: true, message: "Please input your from account" },
+          ]}
+        >
+          <Select
+            // defaultValue="lucy"
+            style={{
+              width: 200,
+            }}
+            onChange={handleChange}
+            showSearch
+            filterOption={
+              (input, option) => option.label === input
+              // (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+            }
+            options={accounts}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Amount"
+          name="Amount"
+          rules={[{ required: true, message: "Please input initial deposit" }]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Done
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default AddTransaction;
